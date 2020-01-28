0fcb4b7ed7d345086a8b65ab09d048f54b16ad3b
tkong9 <taewon2711@gmail.com>
2020-01-28 16:04:55 -0500

    first commit

---
+++ HomeAdvisor/Controller/DetailsViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  DetailsViewCell.swift
+//  HomeAdvisor
+//
+//  Created by TAEWON KONG on 1/27/20.
+//  Copyright © 2020 TAEWON KONG. All rights reserved.
+//
+
+import UIKit
+
+class DetailsViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
---
+++ HomeAdvisor/Model/ProModel.swift
@@ -0,0 +1,13 @@
+//
+//  Pro.swift
+//  HomeAdvisor
+//
+//  Created by TAEWON KONG on 1/27/20.
+//  Copyright © 2020 TAEWON KONG. All rights reserved.
+//
+
+import Foundation
+
+struct Pro: Codable {
+    
+}
